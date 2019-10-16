c75d6b24e42a95bcabbdf41c508b703428031d0b
Ikenna_1246 <pacificheights2008@hotmail.com>
2019-10-16 02:27:10 +0100

    express server setup with initial routes

---
+++ routes/users.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+// ...rest of the initial code omitted for simplicity.
+const { check, validationResult } = require("express-validator");
+
+const User = require("../models/User");
+
+//@route   POST api/users
+// @desc   register a user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please Include a Valid Email").isEmail(),
+    check(
+      "password",
+      "Please Enter Password with 5 or more characters"
+    ).isLength({ min: 5 })
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User Already Exists" });
+      }
+      user = new User({ name, email, password });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (error, token) => {
+          if (error) throw error;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+const app = express();
+const favicon = require("serve-favicon");
+
+// Connect to Database
+connectDB();
+// init middleware
+app.use(express.json({ extended: false }));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+// serve static assets in production
+if (process.env.NODE.ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
